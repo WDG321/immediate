99a91ef6be09f6370dfe49b999ab9e049d09652a
WDG321 <1975208521@qq.com>
2022-12-22 13:12:17 +0800

    第一次提交

---
+++ immd/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ hello }}</div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "App",
+  setup() {
+    let hello = ref("hello world");
+    return { hello };
+  },
+};
+</script>
+
+<style>
+</style>
