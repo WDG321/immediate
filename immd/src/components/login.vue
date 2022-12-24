c980c81e9a67820f6eb543c7b140a634e6d1917b
WDG321 <1975208521@qq.com>
2022-12-24 16:56:57 +0800

    新增登陆页面的输入框

--- immd/src/components/login.vue
+++ immd/src/components/login.vue
@@ -1,5 +1,12 @@
 <template>
-login
+  <div class="logo">
+    <div class="img"></div>
+    <span class="text">IMMD</span>
+  </div>
+  <div class="input">
+    <el-input v-model="input" placeholder="账号" class="username" />
+    <el-input v-model="input" placeholder="密码" class="passworld" />
+  </div>
 </template>
 
 <script>
@@ -7,11 +14,48 @@ import { ref } from "vue";
 export default {
   name: "login",
   setup() {
-
-    return {  };
+    let input = ref("");
+    return { input };
   },
 };
 </script>
 
-<style>
+<style scoped>
+.logo {
+  display: flex;
+  align-items: center;
+  position: absolute;
+  transform: translate(-50%);
+  left: 50%;
+  top: 2rem;
+}
+.img {
+  width: 5rem;
+  height: 5rem;
+  background-image: url(2233.png);
+  background-size: 100%, 100%;
+}
+.text {
+  font-size: 3rem;
+}
+.input {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  transform: translate(-50%);
+  left: 50%;
+  top: 15rem;
+}
+.username {
+  width: 15rem;
+  height: 3.5rem;
+}
+:deep(.el-input__wrapper) {
+  border-radius: 1.5rem;
+}
+
+.passworld {
+  width: 15rem;
+  height: 3.5rem;
+}
 </style>
